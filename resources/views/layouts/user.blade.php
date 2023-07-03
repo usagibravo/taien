9478cf5228ffc6d38dfd6df27a2f6670205227e3
Masahiro Ohno <usagibravo@gmail.com>
2023-07-03 22:34:57 +0900

    google tag追加

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EG4LNB70E5"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-EG4LNB70E5');
+        </script>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
